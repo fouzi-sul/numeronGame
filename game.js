1439fc5c05e3bbb94f2eac89785f6f5523b06edc
fouzi-sul <sfouzia939@gmail.com>
2022-11-08 18:52:37 +0530

    up to game.js

--- game.js
+++ game.js
@@ -1,17 +1,30 @@
 // Generate 2 random numbers, select the dom for numberbox 1 and 2 
+var number1 = Math.round(Math.random()*100)
+let number1box = document.getElementById("number1")
+
+var number2 = Math.round(Math.random()*100)
+let number2box = document.getElementById("number2")
 
 
 var operator;
 var score = 0;
 
 //Select the dom for numberbox 3
+let number3box = document.getElementById("number3")
 
 
 
 function randomise() {
     // generate 2 random numbers again
+     number1 = Math.round(Math.random()*100)
+     number1 = Math.round(Math.random()*100)
     
     // if number 1 is less that number 2 then swap (since we want the result to be positive)
+    if(number1 < number2){
+        var extra = number1
+        number1 = number2
+        number2 = extra
+    }
     
 
     // Generate a random number between 0 to 5:
@@ -23,13 +36,33 @@ function randomise() {
     5. number 3 should be modulous of number 1 and 2
     0. Call the randomise function again
     */
-   operator = Math.round(Math.random() * 5)
+   operator = Math.round(Math.random()*5)
     switch (operator) {
+            case 1:
+                number3 = number1+number2
+            break
+            case 2:
+                number3 = number1-number2
+            break
+            case 3:
+                number3 = number1*number2
+            break
+            case 4:
+                number3 = math.floor(number1/number2)
+            break
+            case 5:
+                number3 = number1%number2
+            break
+            case 0:
+                randomise()
+           
+        }
         
 
-    }
-
     // Place the numbers in their boxes by making use of innerhtml
+    number1box.innerHTML= number1
+    number2box.innerHTML= number2
+    number3box.innerHTML= number3
     
     console.log(number1 + " " + number2 + " " + number3 + " " + operator);
 }
@@ -38,6 +71,11 @@ randomise();
 
 //select the plus, minus,mul,divide,modulus buttons
 
+const plus= document.getElementById("plus")
+const minus= document.getElementById("minus")
+const mul= document.getElementById("mul")
+const divide= document.getElementById("divide")
+const modulus= document.getElementById("modulus")
 
 
 // When these buttons are clicked, check if the selection is correct,
